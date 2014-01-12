d31bb525d6b47ceb9c49cc4a4cb3abba0e3fa2c2
Bennett Kanuka <bkanuka@gmail.com>
2014-01-12 14:11:46 -0500

    added option to generate

--- generate.sh
+++ generate.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
-for i in {1..8}
+
+# Script to generate and print n bingo cards
+n=8
+
+for ((i=1;i<=n;i++))
 do
     ./bingo.sh
     mv bingo.pdf ${i}.pdf
+    # Comment out these lines to disable printing
+    # and deleting
     lp ${i}.pdf
     rm ${i}.pdf
 done
